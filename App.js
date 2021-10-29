e5053736022816f6a45c12402237d872f459eb60
Jonathan Kofi Donkor <50588880+jonathankd9@users.noreply.github.com>
2021-10-29 01:23:17 +0000

    setting up screens

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Login } from "./screens/Login.js";
+import { Home } from "./screens/Home.js";
+import { Cart } from "./screens/Cart.js";
+
+const Stack = createStackNavigator();
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<Text>Open up App.js to start working on your app!</Text>
-			<Text>I TRIED TO MAKE SOME CHANGES!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				{/* <AppStack.Screen name="Login Page" component={Login} />  */}
+				<Stack.Screen
+					options={{
+						header: () => null,
+					}}
+					name="Login"
+					component={Login} 
+          options={{ title: 'Cart' }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
