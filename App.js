b59222c1cd9be5e774ba5f2eeb21a148d410d9c8
Jonathan Kofi Donkor <50588880+jonathankd9@users.noreply.github.com>
2021-10-30 17:39:44 +0000

    update on homescreen

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Login } from "./screens/Login.js";
-import { Home } from "./screens/Home.js";
-import { Cart } from "./screens/Cart.js";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import COLORS from './src/const/colors';
+import { HomeScreen } from './screens/HomeScreen.js';
+import { DetailsScreen } from './screens/DetailsScreen';
 
-const Stack = createStackNavigator();
 
-export default function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				{/* <AppStack.Screen name="Login Page" component={Login} />  */}
-				<Stack.Screen
-					options={{
-						header: () => null,
-					}}
-					name="Login"
-					component={Login} 
-          options={{ title: 'Cart' }}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+const Stack = createStackNavigator()
+
+export default function App () {
+  return (
+    <NavigationContainer>
+	<StatusBar barStyle="dark-content" backgroundColor={COLORS.white}/>
+      <Stack.Navigator screenOptions={{ header: () => null }}>
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{ title: 'HomeScreen' }}
+        />
+        <Stack.Screen
+          name='Details'
+          component={DetailsScreen}
+          options={{ title: 'Details' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
---
+++ src/const/hamper.js
@@ -0,0 +1,11 @@
+const plants = [
+    {
+        id:1,
+        name: 'Hamper 1',
+        price: '19.99',
+        like: true;
+        img: require('../assets/hamperimg/hamper1'),
+        about: 'Beautiful hamper 1'
+    },
+    
+]
