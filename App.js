08a3ba51320201e61f8ff0157ad63ffa2824f346
Jonathan Kofi Donkor <50588880+jonathankd9@users.noreply.github.com>
2021-11-02 14:53:23 +0000

    update on images

--- App.js
+++ App.js
@@ -1,32 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import COLORS from './src/const/colors';
-import { HomeScreen } from './screens/HomeScreen.js';
-import { DetailsScreen } from './screens/DetailsScreen';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "react-native";
+import COLORS from "./src/consts/colors";
+import HomeScreen from "./src/view/screens/HomeScreen";
+import DetailsSceeen from "./src/view/screens/DetailsScreen";
 
+const Stack = createStackNavigator();
 
-const Stack = createStackNavigator()
-
-export default function App () {
-  return (
-    <NavigationContainer>
-	<StatusBar barStyle="dark-content" backgroundColor={COLORS.white}/>
-      <Stack.Navigator screenOptions={{ header: () => null }}>
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{ title: 'HomeScreen' }}
-        />
-        <Stack.Screen
-          name='Details'
-          component={DetailsScreen}
-          options={{ title: 'Details' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+export default function App() {
+	return (
+		<NavigationContainer>
+			<StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+			<Stack.Navigator screenOptions={{ header: () => null }}>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				{/* <Stack.Screen name="Details" component={DetailsSceeen} /> */}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
